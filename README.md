14afb1878a1c65099d5297b0e1c329946c62911c
ken <ken@bigMo>
2015-02-22 13:10:48 -0500

    Check In

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ NodeBlogMean Chapter 11 Learning AngularJS by Ken Williamson
 
 This is NodeBlogMean written for chapter 11 of Learning AngularJS by Ken Williamson.
 
+You will need to add new blog posts when running locally on you development machine. 
+You will also need to modify the Protractor script for chapter 11 to match the new blog posts.
+
+
+
 
--- test/e2e/blog-spec.js
+++ test/e2e/blog-spec.js
@@ -1,5 +1,8 @@
 describe("Blog Application Test", function(){
     it("should test the main blog page", function(){
+        // note: this script will fail if you don't:
+        // 1. add new blog posts 
+        // 2. modify the script to match the blog posts stored in your local MongoDB
         
         browser.get("http://localhost:8080/#!/");
         //log into the blog application
@@ -14,9 +17,10 @@ describe("Blog Application Test", function(){
         var blogList = element.all(by.repeater('blogPost in blogList'));
         
         //test the size of the blogList
+        //change this to the count of blog posts that you added
         expect(blogList.count()).toEqual(3);
         
-        
+        //change this to the id of one of your new blog posts
         browser.get("http://localhost:8080/#!/blogPost/5387bafe185e4e972996adff");
         expect(browser.getTitle()).toEqual("AngularJS Blog");
         
@@ -24,6 +28,7 @@ describe("Blog Application Test", function(){
         var commentList = element.all(by.repeater('comment in blogEntry.comments'));
         
         //checks the size of the commentList
+        //change this to the number of blog comments for the blog post above on your local system
         expect(commentList.count()).toEqual(2);
         
         element(by.css('.navbar-brand')).click();
